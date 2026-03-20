e263cf7938e0e3725c0809c68a1b3d7297f86722
hojong <33508056+LteFroggy@users.noreply.github.com>
2026-03-20 15:00:28 +0900

    chore : GameObject 내부 코드 수정

--- Engine/GameObject.cs
+++ Engine/GameObject.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Drawing;
 
 namespace Framework.Engine
 {
@@ -7,14 +8,16 @@ namespace Framework.Engine
         protected float _xLoc;
         protected float _yLoc;
 
-        public abstract int XLoc { get; }
-        public abstract int YLoc { get; }
-        public abstract int Width { get; }
-        public abstract int Height { get; }
+        protected virtual ConsoleColor Color { get; }
 
         // 장애물 충돌 판정 거리
-        public abstract int CollisionWidth { get; }
-        public abstract int CollisionHeight { get; }
+        public virtual int CollisionWidth { get; }
+        public virtual int CollisionHeight { get; }
+
+        public abstract int XLoc { get; }
+        public abstract int YLoc { get; }
+        public abstract int ObjectWidth { get; }
+        public abstract int ObjectHeight { get; }
 
         public string Name { get; set; } = "";
         public bool IsActive { get; set; } = true;
