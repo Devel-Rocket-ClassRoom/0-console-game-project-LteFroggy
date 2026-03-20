569c7a04c62ae1e3c72c82a84d9d762a4a99414a
hojong <33508056+LteFroggy@users.noreply.github.com>
2026-03-20 15:01:02 +0900

    chore : 변수명 변경 및 WhiteSpace 수정

--- Game/Scenes/PlayScene.cs
+++ Game/Scenes/PlayScene.cs
@@ -6,44 +6,59 @@ namespace Framework.MyGame
 {
     public class PlayScene : Scene
     {
+        private List<GameObject> _obstacles;
+        private List<GameObject> _pendingRemovalObstacles;
+        private GameObject _player;
         private GameState _gameState = GameState.Playing;
         private int _score;
+        // 게임 종료 시 출력될 스트링 (차에 치었습니다 등)
+        private string _gameOverString;
 
         // 장애물 생성용 팩토리클래스
         private ObstacleFactory _obstacleFactory;
-        // 게임 종료 시 출력될 스트링 (차에 치었습니다 등)
-        private string _gameOverString;
+        // 배경 지형지물 생성용 팩토리클래스
+        private BackgroundFactory _backgroundFactory;
         
         // 게임 출력 화면 너비 (지면 렌더링 시 사용)
-        private int _width;
+        private readonly int _width;
         // 게임 출력 화면 높이 (해, 달 등 오브젝트 렌더링 시 사용)
-        private int _height;
+        private readonly int _height;
         // 장애물 스폰 타이머
         private float _spawnTimer;
         private float _nextSpawnTime;
 
-        private Random _rand = new Random();
+        // 배경 스폰 차례값
+        private int _backgroundPhase = default;
+        // 배경 스폰 관련값
+        private const int k_SunSpawnScore = 0;
+        private const int k_CloudSpawnPeriod = 2;
+        private const int k_MoonSpawnScore = 12;
+        private const int k_BackgroundCycle = 24;
+        private bool _isCloudSpawned = false;
 
+        private readonly Random _rand = new Random();
         private float _acceleration => 30f + _score * 2;
+
         public event GameAction PlayAgainRequested;
         public event GameAction BackToMain;
         
-        public PlayScene(int width, int height, ObstacleFactory factory) {
+        public PlayScene(int width, int height, ObstacleFactory factory, BackgroundFactory backgroundFactory) {
             _score = 0;
             _width = width;
             _height = height;
 
             _obstacleFactory = factory;
+            _backgroundFactory = backgroundFactory;
+
+            _pendingRemovalObstacles = new List<GameObject>();
+            _obstacles = new List<GameObject>();
         }
 
         public override void Load()
         {
-            AddGameObject(new Dinosour(this)); 
-            ObstacleRemoveEvent += (AddScore);
-        }
-
-        private void AddScore() {
-            _score++;
+            GameObject player = new Dinosour(this);
+            _player = player;
+            AddGameObject(player); 
         }
 
         public override void Update(float deltaTime)
@@ -51,12 +66,18 @@ namespace Framework.MyGame
             if (_gameState == GameState.Playing) {
                 _spawnTimer += deltaTime;
                 UpdateGameObjects(deltaTime, _acceleration);
+
+                // 지형지물 스폰
+                SpawnBackground();
             
                 // 장애물 스폰
-                TrySpawn();
+                SpawnObstacle();
 
                 // 충돌체크
                 CheckCollision();
+
+                // 넘어간 장애물 삭제
+                DespawnObstacle();
             }
 
             // 게임 오버 후라면, 업데이트는 중지
@@ -78,7 +99,7 @@ namespace Framework.MyGame
             // 가로줄 그리기 (바닥)
             buffer.DrawHLine(0, 19, _width, '-', ConsoleColor.DarkYellow);
             // 점수판
-            buffer.WriteText(1, 1, $"점수 : {_score}점");
+            buffer.WriteText(0, 0, $"넘은 장애물 수 : {_score}개");
             // 오브젝트
             DrawGameObjects(buffer);
 
@@ -90,37 +111,87 @@ namespace Framework.MyGame
             }
         }
 
-        public override void Unload()
-        {
+        public override void Unload() {
             ClearGameObjects();
         }
 
-        private void TrySpawn() {
+        private void SpawnObstacle() {
             // 스폰시간이 되었다면, 장애물 추가
             if (_spawnTimer >= _nextSpawnTime) {
                 // 랜덤 오브젝트 생성
-                AddGameObject(_obstacleFactory.GetRandomObstacle(_rand.Next(1, 100), this, _width, _height));
+                GameObject obj = _obstacleFactory.GetRandomObstacle(this, _width, _height);
+                AddGameObject(obj);
+                AddObstacle(obj);
                 _spawnTimer = 0;
-                _nextSpawnTime = ( _score * 0f) + 1.5f + (float)(_rand.NextDouble() * 2f);
-            } else { }
+                _nextSpawnTime = 1.5f + (float)(_rand.NextDouble() * 2f);
+            }
+            // 시간이 되지 않았다면, 스킵
+            else { }
+        }
+
+        private void SpawnBackground() {
+            // 18점마다 사이클 반복
+            int _scoreInCycle = _score % k_BackgroundCycle;
+
+            // 해 스폰, 달 스폰 시점 제외 구름 반복 생성
+            if (!_isCloudSpawned
+                && _scoreInCycle % k_CloudSpawnPeriod == 0 
+                && _scoreInCycle != k_SunSpawnScore 
+                && _scoreInCycle != k_MoonSpawnScore) {
+                AddGameObject(_backgroundFactory.MakeCloud(this, _width, _height));
+                _isCloudSpawned = true;
+            } else if (_scoreInCycle % k_CloudSpawnPeriod != 0) {
+                _isCloudSpawned = false;
+            }
+            // 해 등장
+            if (_backgroundPhase == 0 
+                    && _scoreInCycle >= k_SunSpawnScore) {
+                AddGameObject(_backgroundFactory.MakeSun(this, _width, _height));
+                _backgroundPhase = 1;
+            } 
+            // 달 등장
+            else if (_backgroundPhase == 1 
+                    && _scoreInCycle >= k_MoonSpawnScore) {
+                AddGameObject(_backgroundFactory.MakeMoon(this, _width, _height));
+                _backgroundPhase = 2;
+            }
+            // 사이클 초기화
+            if (_scoreInCycle == 0 && _backgroundPhase == 2) {
+                _backgroundPhase = 0;
+            }
+        }
+
+        private void DespawnObstacle() {
+            // 공룡 위치와 나머지를 모두 비교
+            foreach (var obstacle in _obstacles) {
+                // inActive인 값은 삭제할 것
+                if (!obstacle.IsActive) {
+                    _pendingRemovalObstacles.Add(obstacle);
+                    continue;
+                }
+            }
+
+            // 삭제
+            foreach (var target in _pendingRemovalObstacles) {
+                _obstacles.Remove(target);
+                _score++;
+            }
+            _pendingRemovalObstacles.Clear();
         }
 
         private void CheckCollision() {
-            IReadOnlyList<GameObject> objects = GameObjects;
-
             // 공룡의 현위치 파악
-            int dinoXStart = objects[0].XLoc;
-            int dinoXEnd = dinoXStart + objects[0].CollisionWidth;
-            int dinoYStart = objects[0].YLoc;
-            int dinoYEnd = dinoYStart + objects[0].CollisionHeight;
+            int dinoXStart = _player.XLoc;
+            int dinoXEnd = dinoXStart + _player.CollisionWidth;
+            int dinoYStart = _player.YLoc;
+            int dinoYEnd = dinoYStart + _player.CollisionHeight;
 
-            // 0번이 공룡, 나머지는 모두 장애물임. 따라서 0번과 나머지만 비교
-            for (int i = 1; i < objects.Count; i++) {
-                int obstacleXStart = objects[i].XLoc;
-                int obstacleXEnd = obstacleXStart + objects[i].CollisionWidth;
-
-                int obstacleYStart = objects[i].YLoc;
-                int obstacleYEnd = obstacleYStart + objects[i].CollisionHeight;
+            // 공룡 위치와 장애물 위치 비교
+            foreach (var obstacle in _obstacles) {
+                int obstacleXStart = obstacle.XLoc;
+                int obstacleXEnd = obstacleXStart + obstacle.CollisionWidth;
+                int obstacleYStart = obstacle.YLoc;
+                int obstacleYEnd = obstacleYStart + obstacle.CollisionHeight;
 
                 // 공룡이 장애물보다 왼쪽에 있는 경우
                 if (dinoXEnd < obstacleXStart) { continue; }
@@ -131,12 +202,16 @@ namespace Framework.MyGame
                 // 공룡이 장애물보다 아래에 있는 경우
                 else if (dinoYStart > obstacleYEnd) { continue; }
                 // 다 아니라면 충돌
-                else {
-                    _gameState = GameState.GameOver;
-                    _gameOverString = ((BasicObstacle)objects[i]).GameOverString;
-                    break;
-                }
+                //else {
+                //    _gameState = GameState.GameOver;
+                //    _gameOverString = ((BasicObstacle)obstacle).GameOverString;
+                //    break;
+                //}
             }
+        }
+
+        private void AddObstacle(GameObject obstacle) {
+            _obstacles.Add(obstacle);
         }
     }
 }
